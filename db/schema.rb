4927fc5e41ef9dd3de1b838258fe7edf46e0e6f1
芝峰克昌 <katsuma.fishing@gmail.com>
2021-07-16 18:00:23 +0900

    boardを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_094958) do
+ActiveRecord::Schema.define(version: 2021_07_16_084559) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
